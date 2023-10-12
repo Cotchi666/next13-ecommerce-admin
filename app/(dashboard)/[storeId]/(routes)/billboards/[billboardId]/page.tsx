14c9a4d43804d4ff2e419540775607bf05d9b658
Cotchi666 <chien.170901@gmail.com>
2023-10-12 13:36:18 +0700

    fix(page.tsx): handle error when fetching billboard data and add condition to check length of billboardId before fetching data fix(schema.prisma): change database provider from mysql to mongodb to match the actual database being used

--- app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -3,23 +3,30 @@ import prismadb from "@/lib/prismadb";
 import { BillboardForm } from "./components/billboard-form";
 
 const BillboardPage = async ({
-  params
+  params,
 }: {
-  params: { billboardId: string }
+  params: { billboardId: string };
 }) => {
-  const billboard = await prismadb.billboard.findUnique({
-    where: {
-      id: params.billboardId
-    }
-  });
+  let billboard: any;
 
-  return ( 
+  try {
+    if (params.billboardId.length > 4) {
+      billboard = await prismadb.billboard.findUnique({
+        where: {
+          id: params.billboardId,
+        },
+      });
+    } else {
+    }
+  } catch (error) {}
+
+  return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <BillboardForm initialData={billboard} />
       </div>
     </div>
   );
-}
+};
 
 export default BillboardPage;
